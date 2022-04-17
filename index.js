65bc4d17fdf01d6d1d717a2e08855fff37fefd19
Pereira da Silva <m.f.pereira.da.silva@accenture.com>
2022-04-17 09:25:03 -0300

    Projeto inicial

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const PORT = 3010
+
+app.get('/',(req,res)=>{
+    res.send("<h1>Hello accenture</h1>")
+})
+
+app.listen(PORT, () => {
+    console.log(`app listening on port ${PORT}`)
+})
